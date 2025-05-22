42d9cf0be2257cce632608e55ecd6bc239688aab
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2025-05-22 12:09:50 +0200

    hero section added.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { navlinks } from '../constants/constant'
+import { FiMenu, FiX } from 'react-icons/fi'
 
 function Navbar() {
-    return (
-        <div className='nav flex h-[70px] bg-white h-[80px] justify-between'>
-            <div className="flex justify-center items-center text-[25px] flex-[0.5] bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent">FinTrack</div>
-            <ul className="hidden lg:flex flex-1 items-center justify-evenly">
-                {navlinks.map((link, index) => (
-                    <li key={index}>
-                        <a href={link.url} className='text-[15px] text-gray-400'>{link.name}</a>
-                    </li>
-                ))
+    const [toggle, setToggle] = useState(false);
 
-                }
-            </ul>
-            <div className="hidden lg:flex flex-[0.5] items-center justify-center">
-                <button className=' w-[100px] h-[40px] bg-blue-500 rounded border-1 border-blue-500 text-white hover:text-blue-500 hover:bg-white mr-10 '>
-                        Sign In
-                </button>
-                <button className=' w-[100px] h-[40px] border-1 border-blue-500 rounded text-blue-500 hover:bg-blue-500 hover:text-white'>
-                        Sign Up
-                </button>
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
+    return (
+        <>
+            <div className='nav flex h-[70px] bg-white h-[80px] justify-between border-1 border-gray-200'>
+                <div className="flex justify-center items-center text-[25px] flex-[0.5] bg-gradient-to-r from-blue-600 to-blue-800 bg-clip-text text-transparent font-[600]">finTrack</div>
+                <ul className="hidden lg:flex flex-1 items-center justify-evenly">
+                    {navlinks.map((link, index) => (
+                        <li key={index}>
+                            <a href={link.url} className='text-[15px] text-gray-400'>{link.name}</a>
+                        </li>
+                    ))
+
+                    }
+                </ul>
+                <div className="hidden lg:flex flex-[0.5] items-center justify-center">
+                    <button className=' w-[100px] h-[40px] bg-blue-500 rounded-[20px] border-1 border-blue-500 text-white hover:text-blue-500 hover:bg-white mr-10 '>
+                        Sign in
+                    </button>
+                    <button className=' w-[100px] h-[40px] border-1 border-blue-500 rounded-[20px] text-blue-500 hover:bg-blue-500 hover:text-white'>
+                        Sign up
+                    </button>
+                </div>
+
+                <div className="flex lg:hidden justify-center items-center mr-[20px] text-[20px]" onClick={handleToggle}>
+                    {toggle ? <FiX /> : <FiMenu />}
+                </div>
+
+
+
             </div>
 
-        </div>
+            {toggle &&
+                <div className='bg-white flex flex-col absolute w-100'>
+                <ul className="lg:hidden flex flex-col items-center justify-evenly  mt-1">
+                    {navlinks.map((link, index) => (
+                        <li key={index} className='my-[10px]'>
+                            <a href={link.url} className='text-[15px] text-gray-400'>{link.name}</a>
+                        </li>
+                    ))
+                    }
+                </ul>
+                <div className="lg:hidden flex  items-center justify-center my-[10px]">
+                    <button className=' w-[100px] h-[40px] bg-blue-500 rounded-[20px] border-1 border-blue-500 text-white hover:text-blue-500 hover:bg-white mr-10 '>
+                        Sign in
+                    </button>
+                    <button className=' w-[100px] h-[40px] border-1 border-blue-500 rounded-[20px] text-blue-500 hover:bg-blue-500 hover:text-white'>
+                        Sign up
+                    </button>
+                </div>
+                </div>
+
+            }
+        </>
+
     )
 }
 
---
+++ frontend/src/images/index.js
@@ -0,0 +1,6 @@
+import hero from './hero.png'
+
+
+export {
+    hero,
+}
