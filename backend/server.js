cb1e20b039672f13d2cdabe73e1ebbb23f117341
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2025-05-27 19:41:40 +0200

    backend file structure done.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-});
+app.use(cors());
+app.use(express.json());
+
+try {
+    const authRoute = require('./routes/auth');
+    app.use('/auth', authRoute);
+}
+catch (e) {
+    console.log(e)
+};
+
 
 app.listen(3001, ()=> {
     console.log("App is runing on port 3001")
